3f300dd78f15f9e64547a1ac34748f3a175d7b14
Cody Tanner <cody.tanner@getbridge.com>
2024-03-18 07:44:12 -0600

    Update matchup_selections_controller.rb

--- app/controllers/matchup_selections_controller.rb
+++ app/controllers/matchup_selections_controller.rb
@@ -1,4 +1,5 @@
 class MatchupSelectionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_action :set_matchup_selection, only: %i[ show update destroy ]
 
   def show
